d30f7ad054e678d9910b36987bf2acdb0fd5bcf6
artketler <artketler.ak@gmail.com>
2024-03-04 19:02:16 +0300

    HomeWork_#5/1 Complete

--- src/main/java/ru/geekbrains/core/lesson4/seminar/task1/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.core.lesson4.seminar.task1;
-
-public class MyArrayDataException extends CustomArrayException{
-    public MyArrayDataException(String message, int x, int y) {
-        super(message, x, y);
-    }
-}
